0b8d4439310f2eaceafcbc3699d0aea553115a8b
Tyler Madonna <TJMadonna88@gmail.com>
2017-02-11 14:33:02 -0500

    Modified getForecast method so client can get the forecast for city name or city id

--- Sunshine/Model/OpenWeatherConvenience.swift
+++ Sunshine/Model/OpenWeatherConvenience.swift
@@ -12,10 +12,44 @@ import Foundation
 
 extension OpenWeatherClient {
     
-    func getForecast(completionHandlerForForecast: @escaping (_ results: [DailyForecast]?, _ error: NSError?) -> Void) {
+    func getForecastFor(cityName: String, completionHandlerForForecast: @escaping (_ results: [DailyForecast]?, _ error: NSError?) -> Void) {
         
         let parameters: [String: String] = [
-            ParameterKeys.CityName: "Pittsburgh",
+            ParameterKeys.CityName: cityName,
+            ParameterKeys.Format: Constants.JSONFormat,
+            ParameterKeys.NumberOfDays: String(14),
+            ParameterKeys.Units: Constants.ImperialUnits,
+            ]
+        
+        taskForGETMethod(method: Methods.DailyForecast, parameters: parameters) { (result, error) in
+            
+            guard error == nil else {
+                performOnMainQueue(updates: {
+                    completionHandlerForForecast(nil, error)
+                })
+                return
+            }
+            
+            if let result = result as? [String: Any], let forecastDictionary = result["list"] as? [[String: AnyObject]] {
+                
+                let forecast = DailyForecast.dailyForecastsFromResults(results: forecastDictionary)
+                
+                performOnMainQueue(updates: {
+                    completionHandlerForForecast(forecast, nil)
+                })
+                
+            } else {
+                performOnMainQueue(updates: {
+                    completionHandlerForForecast(nil, NSError(domain: "Forecast parsing", code: 0, userInfo: [NSLocalizedDescriptionKey: "Could not parse forecast"]))
+                })
+            }
+        }
+    }
+    
+    func getForecastFor(cityId: Int, completionHandlerForForecast: @escaping (_ results: [DailyForecast]?, _ error: NSError?) -> Void) {
+        
+        let parameters: [String: String] = [
+            ParameterKeys.CityId: String(cityId),
             ParameterKeys.Format: Constants.JSONFormat,
             ParameterKeys.NumberOfDays: String(14),
             ParameterKeys.Units: Constants.ImperialUnits,

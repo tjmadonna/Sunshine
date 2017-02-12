27c897d782f208eb80c3997342ed84742c60c66c
Tyler Madonna <TJMadonna88@gmail.com>
2017-02-12 11:37:11 -0500

    Create core data stack reference

--- Sunshine/AppDelegate.swift
+++ Sunshine/AppDelegate.swift
@@ -12,6 +12,8 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    let coreDataStack = CoreDataStack()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         

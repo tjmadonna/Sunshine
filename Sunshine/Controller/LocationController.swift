ee3ef8cc0f51c68bf7b3703eaf32ac29d27d1ec1
Tyler Madonna <TJMadonna88@gmail.com>
2017-02-12 11:31:16 -0500

    Created LocationController and added location icon

---
+++ Sunshine/Controller/LocationController.swift
@@ -0,0 +1,13 @@
+//
+//  LocationController.swift
+//  Sunshine
+//
+//  Created by Tyler Madonna on 2/11/17.
+//  Copyright © 2017 Tyler Madonna. All rights reserved.
+//
+
+import UIKit
+
+class LocationController: CoreDataController {
+
+}
